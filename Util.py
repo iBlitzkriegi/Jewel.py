c4f1ee31439809387a499b457c25f433dceaf810
iBlitzkriegi <junowars5@yahoo.com>
2019-03-18 16:38:05 -0400

    Add function to print errors in a noticeable format

--- Util.py
+++ Util.py
@@ -1,2 +1,10 @@
 def get_size(list):
     return str(len(list))
+
+
+def print_error(string):
+    error = []
+    error.append('~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~')
+    error.append(string)
+    error.append('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print("\n".join(error))
