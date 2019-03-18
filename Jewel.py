dcb5651334dac5ef65804e7fb93b98c3826c5ff7
iBlitzkriegi <junowars5@yahoo.com>
2019-03-18 16:40:26 -0400

    Read from config for token

--- Jewel.py
+++ Jewel.py
@@ -1,20 +1,28 @@
 from commands.general import GeneralCommands
 from commands.staff import StaffCommands
-from Util import get_size
+import Util
 from discord.ext import commands
 
 description = 'Jewel, the magical bot!'
+token = None
 
 bot = commands.Bot(command_prefix='!', description='Jewel the magical Discord bot')
 bot.add_cog(StaffCommands.Staff(bot))
 bot.add_cog(GeneralCommands.General(bot))
 
+config = open('./ignore/config.yml')
+for s in config:
+    token = s
+
 
 @bot.event
 async def on_ready():
-    print('----')
-    print(bot.user.name + ' is up and ready to serve ' + get_size(bot.guilds) + ' guilds!')
-    print('----')
+    print('------------------------------------------------')
+    print(bot.user.name + ' is up and ready to serve ' + Util.get_size(bot.guilds) + ' guilds!')
+    print('------------------------------------------------')
 
-
-bot.run('')
+if token is not None:
+    bot.run(token)
+    token = None
+else:
+    Util.print_error('Could not parse config file! Could be missing or corrupt.')
