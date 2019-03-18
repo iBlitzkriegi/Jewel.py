b641512d3af94c6e837245eecf91c25891dd6d9d
iBlitzkriegi <junowars5@yahoo.com>
2019-03-18 17:45:41 -0400

    Added is_owner function that check for staff commands

--- commands/staff/StaffCommands.py
+++ commands/staff/StaffCommands.py
@@ -1,6 +1,13 @@
 from discord.ext import commands
 
 
+def is_owner():
+    async def predicate(ctx):
+        return ctx.author.id == 98208218022428672
+
+    return commands.check(predicate)
+
+
 class Staff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,8 +17,8 @@ class Staff(commands.Cog):
         aliases=['shutdown'],
         description='This is a command for staff only to stop the bot'
     )
+    @is_owner()
     async def stop_bot(self, ctx):
         """Shutdown the bot"""
-        if ctx.author.id == 98208218022428672:
-            await ctx.send('Oh, alright... I\'ll just shutup I guess.. :wave:')
-            await self.bot.close()
+        await ctx.send('Oh, alright... I\'ll just shutup I guess.. :wave:')
+        await self.bot.close()
